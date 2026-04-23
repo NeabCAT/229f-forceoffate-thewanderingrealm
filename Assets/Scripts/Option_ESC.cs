d9904e5d2745416360fcef56dba13e652a2ce10c
MalineeSunrise <malineebukong@gmail.com>
2026-04-23 23:10:05 +0700

    Fix-cs-Option_Sound

--- Assets/Scripts/Option_ESC.cs
+++ Assets/Scripts/Option_ESC.cs
@@ -12,6 +12,21 @@ public class Option_ESC : MonoBehaviour
     public Slider SFX_Vol;
     public AudioMixer mainAuio;
 
+    void Start()
+    {
+        float music = PlayerPrefs.GetFloat("Music_Vol", 1f);
+        float sfx = PlayerPrefs.GetFloat("SFX_Vol", 1f);
+
+        Music_Vol.value = music;
+        SFX_Vol.value = sfx;
+
+        float musicdB = Mathf.Log10(Mathf.Max(0.0001f, music)) * 20;
+        float sfxdB = Mathf.Log10(Mathf.Max(0.0001f, sfx)) * 20;
+
+        mainAuio.SetFloat("Music_Vol", musicdB);
+        mainAuio.SetFloat("SFX_Vol", sfxdB);
+    }
+
     void Update()
     {
         ESCOpen();
@@ -30,13 +45,21 @@ public class Option_ESC : MonoBehaviour
 
     public void ChangeMusicVolume()
     {
+        float value = Music_Vol.value;
+
         float dB = Mathf.Log10(Mathf.Max(0.0001f, Music_Vol.value)) * 20;
         mainAuio.SetFloat("Music_Vol", dB);
+
+        PlayerPrefs.SetFloat("Music_Vol", value);
     }
     public void ChangeSFXVolume()
     {
+        float value = SFX_Vol.value;
+
         float dB = Mathf.Log10(Mathf.Max(0.0001f, SFX_Vol.value)) * 20;
         mainAuio.SetFloat("SFX_Vol", dB);
+
+        PlayerPrefs.SetFloat("SFX_Vol", value);
     }
 
     public void ESCOpen()

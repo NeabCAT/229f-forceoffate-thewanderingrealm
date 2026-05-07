18227652ec00fd093f77656ded90a607cdaa8be4
MalineeSunrise <malineebukong@gmail.com>
2026-05-08 00:29:41 +0700

    muteSound

--- Assets/Scripts/Option_ESC.cs
+++ Assets/Scripts/Option_ESC.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Option_ESC : MonoBehaviour
 {
@@ -13,6 +14,13 @@ public class Option_ESC : MonoBehaviour
     public AudioMixer mainAuio;
 
     public GameObject uiHp;
+    public GameObject logo;
+    public TextMeshProUGUI nameGame;
+
+    public Toggle muteToggle;
+
+    float lastMusicVolume;
+    float lastSFXVolume;
 
     void Start()
     {
@@ -36,13 +44,17 @@ public class Option_ESC : MonoBehaviour
 
     public void Option()
     {
-        uiOption.SetActive(true);
+        if (uiOption != null) uiOption.SetActive(true);
+        if (logo != null) logo.SetActive(false);
+        if (nameGame != null) nameGame.gameObject.SetActive(false);
     }
 
     public void Back()
     {
         Time.timeScale = 1f;
-        uiOption.SetActive(false);
+        if (uiOption != null) uiOption.SetActive(false);
+        if (logo != null) logo.SetActive(true);
+        if (nameGame != null) nameGame.gameObject.SetActive(true);
     }
 
     public void ChangeMusicVolume()
@@ -66,27 +78,52 @@ public class Option_ESC : MonoBehaviour
 
     public void ESCOpen()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            Time.timeScale = 0f;
-            uiESC.SetActive(true);
-
-            uiHp.SetActive(false);
-        }
-        
+        if (uiESC == null) return;
+        Time.timeScale = 0f;
+        uiESC.SetActive(true);
+        if (uiHp != null) uiHp.SetActive(false);
     }
 
     public void ESCBack()
     {
         Time.timeScale = 1f;
-        uiESC.SetActive(false);
-        uiHp.SetActive(true);
+        if (uiESC != null) uiESC.SetActive(false);
+        if (uiHp != null) uiHp.SetActive(true);
     }
+
 
     public void BackToMainMenu()
     {
         Time.timeScale = 1f;
         SceneManager.LoadScene("MainMenu");
+    }
+
+    public void ToggleAllSound()
+    {
+        if (muteToggle.isOn)
+        {
+            // จำค่าก่อน mute
+            lastMusicVolume = Music_Vol.value;
+            lastSFXVolume = SFX_Vol.value;
+
+            // เลื่อน slider ลง 0
+            Music_Vol.value = 0;
+            SFX_Vol.value = 0;
+
+            // mute เสียง
+            mainAuio.SetFloat("Music_Vol", -80f);
+            mainAuio.SetFloat("SFX_Vol", -80f);
+        }
+        else
+        {
+            // คืนค่า slider เดิม
+            Music_Vol.value = lastMusicVolume;
+            SFX_Vol.value = lastSFXVolume;
+
+            // คืนเสียงตาม slider
+            ChangeMusicVolume();
+            ChangeSFXVolume();
+        }
     }
 
 }
